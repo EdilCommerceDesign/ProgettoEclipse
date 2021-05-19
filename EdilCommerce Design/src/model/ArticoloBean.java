ac411f4528dbbee19d4c1ea326fda8456fb83d2b
luigidechiara <luigidechiara00@gmail.com>
2021-05-19 16:48:52 +0200

    articoloBean

---
+++ EdilCommerce Design/src/model/ArticoloBean.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.sql.Blob;
+
+public class ArticoloBean {
+	String codiceArticolo;		
+	String nome;				
+	Blob immagine;			
+	String descrizione;		
+	double costo;				
+	String nomeCategoria;
+	
+	public ArticoloBean(){
+		codiceArticolo= "";
+		nome= "";
+		immagine= null;
+		descrizione= "";
+		costo= 0;
+		nomeCategoria= "";
+	}
+
+	public String getCodiceArticolo() {
+		return codiceArticolo;
+	}
+
+	public void setCodiceArticolo(String codiceArticolo) {
+		this.codiceArticolo = codiceArticolo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Blob getImmagine() {
+		return immagine;
+	}
+
+	public void setImmagine(Blob immagine) {
+		this.immagine = immagine;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+	
+	public boolean isEmpty() {
+		return codiceArticolo == "";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return this.getCodiceArticolo() ==  ((ArticoloBean) obj).getCodiceArticolo() ;
+	}
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return nome+","+"("+ codiceArticolo +")" +", "+ immagine +", "+ descrizione+", " + costo +", "+ nomeCategoria;
+	}
+	
+	
+	
+}
